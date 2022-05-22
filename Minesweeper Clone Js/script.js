bd42f28a80cfef6933ef31d3e3ddf4f66d1b811b
Hiticas Mihai <hiticasmihai7@gmail.com>
2022-05-22 16:07:48 +0300

    summary
    last commit

---
+++ Minesweeper Clone Js/script.js
@@ -0,0 +1,40 @@
+import {
+  createBoard,
+  markTile,
+  TILE_STATUSES,
+  revealTile,
+} from './minesweeper.js';
+
+const BOARD_SIZE = 10;
+const NUMBER_OF_MINES = 10;
+
+const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES);
+const boardElement = document.querySelector('.board');
+const minesLeftText = document.querySelector('[data-mine-count]');
+
+board.forEach(row => {
+  row.forEach(tile => {
+    boardElement.append(tile.element);
+    tile.element.addEventListener('click', () => {
+      revealTile(board, tile);
+    });
+    tile.element.addEventListener('contextmenu', e => {
+      e.preventDefault();
+      markTile(tile);
+      listMinesLeft();
+    });
+  });
+});
+
+boardElement.style.setProperty('--size', BOARD_SIZE);
+minesLeftText.textContent = NUMBER_OF_MINES;
+
+function listMinesLeft() {
+  const markedTilesCount = board.reduce((count, row) => {
+    return (
+      count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+    );
+  }, 0);
+
+  minesLeftText.textContent = NUMBER_OF_MINES - markedTilesCount;
+}
